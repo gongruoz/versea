2a961d4f00c46e207cd761ec3ee1f8fb95e55ac2
Hazel Ruozhen Gong <ruozhen7@gmail.com>
2024-09-15 23:41:54 +0800

    Initial Commit

---
+++ Versea/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Versea
+//
+//  Created by Hazel Gong on 2024/9/15.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Versea/VerseaApp.swift
@@ -0,0 +1,17 @@
+//
+//  VerseaApp.swift
+//  Versea
+//
+//  Created by Hazel Gong on 2024/9/15.
+//
+
+import SwiftUI
+
+@main
+struct VerseaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
